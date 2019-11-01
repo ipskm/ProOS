1d9cbc671f38e99fae5b1daffefc04ef0bb0c51d
ipskm <ipskm@outlook.com>
2019-11-01 22:25:01 +0700

    กำลังทำอันใหม่นะครับ อันเก่ามันไม่ตรงโจทย์

--- src/rework_new_prodcons.c
+++ src/rework_new_prodcons.c
@@ -1,111 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-struct buffer {
-    int size;
-    int start;
-    int count; 
-    void **element;
-};
- 
-typedef struct buffer buffer_t;
- 
-void init(buffer_t *buffer, int size) {
-    buffer->size = size;
-    buffer->start = 0;
-    buffer->count = 0;
-    buffer->element = malloc(sizeof(buffer->element)*size);
-    /* allocated array of void pointers. Same as below */
-    //buffer->element = malloc(sizeof(void *) * size);
-     
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+/*
+use the pthread flag with gcc to compile this code
+~$ gcc -pthread producer_consumer.c -o producer_consumer
+*/
+
+
+pthread_t *producers;
+pthread_t *consumers;
+
+sem_t buf_mutex,empty_count,fill_count;
+
+int *buf,buf_pos=-1,prod_count,con_count,buf_len;
+
+
+int produce(pthread_t self){
+	int i = 0;
+	int p = 1 + rand()%40;
+	while(!pthread_equal(*(producers+i),self) && i < prod_count){
+		i++;
+	}
+	printf("Producer %d produced %d \n",i+1,p);
+	return p;
 }
- 
-int full(buffer_t *buffer) {
-    if (buffer->count == buffer->size) { 
-        return 1;
-    } else {
-        return 0;
-    }
+
+
+void consume(int p,pthread_t self){
+	int i = 0;
+	while(!pthread_equal(*(consumers+i),self) && i < con_count){
+		i++;
+	}
+
+	printf("Buffer:");
+	for(i=0;i<=buf_pos;++i)
+		printf("%d ",*(buf+i));
+	printf("\nConsumer %d consumed %d \nCurrent buffer len: %d\n",i+1,p,buf_pos);
+	
 }
- 
-int empty(buffer_t *buffer) {
-    if (buffer->count == 0) {
-        return 1;
-    } else {
-        return 0;
-    }
+
+
+void* producer(void *args){
+
+	while(1){
+		int p = produce(pthread_self());
+		sem_wait(&empty_count);
+		sem_wait(&buf_mutex);
+		++buf_pos;			// critical section
+		*(buf + buf_pos) = p; 
+		sem_post(&buf_mutex);
+		sem_post(&fill_count);
+		sleep(1 + rand()%3);
+	}
+	
+	return NULL;
 }
-     
-void push(buffer_t *buffer, void *data) {
-    int index;
-    if (full(buffer)) {
-        printf("Buffer overflow\n");
-    } else {
-        index = buffer->start + buffer->count++;
-        if (index >= buffer->size) {
-            index = 0;
-        }
-        buffer->element[index] = data;
-    }
+
+
+void* consumer(void *args){
+	int c;
+	while(1){
+		sem_wait(&fill_count);
+		sem_wait(&buf_mutex);
+		c = *(buf+buf_pos);
+		consume(c,pthread_self());
+		--buf_pos;
+		sem_post(&buf_mutex);
+		sem_post(&empty_count);
+		sleep(1+rand()%5);
+	}
+
+	return NULL;
 }
- 
- 
-void * popqueue(buffer_t *buffer) {
-    void * element;
-    if (empty(buffer)) {
-        printf("Buffer underflow\n");
-        return "0";
-    } else {
-       /* FIFO implementation */
-       element = buffer->element[buffer->start];
-       buffer->start++;
-       buffer->count--;
-       if (buffer->start == buffer->size) {
-           buffer->start = 0;
-       }
-        
-       return element;
-    }
-}
- 
-void * popstack(buffer_t *buffer) {
-    int index;
-    if (empty(buffer)) {
-        printf("Buffer underflow\n");
-        return "0";
-    } else {
-        /* LIFO implementation */
-        index = buffer->start + buffer->count - 1;
-        if (index >= buffer->size) {
-           index = buffer->count - buffer->size - 1;
-        }      
-        buffer->count--;
-        return buffer->element[index];
-    }
-}
- 
-int main() {
-    buffer_t buffer;
-    init(&buffer, 5);
-    char *data[] = {"first" , "second", "third", "fourth" , "fifth", "sixth"};
-    int i;
-    for (i = 0; i < 6; i++) {
-        printf("push: %s\n", data[i]);
-        push(&buffer, data[i]);
-    }
-    printf("\n");
-    for (i = 0; i < 6; i++) {
-        /* need to cast void pointers to char pointers */
-        printf("pop from queue: %s\n", (char *)popqueue(&buffer));
-    }
-     
-    for (i = 0; i < 6; i++) {
-        printf("push: %s\n", data[i]);
-        push(&buffer, data[i]);
-    }
-    for (i = 0; i < 6; i++) {
-        printf("pop from stack: %s\n", (char *)popstack(&buffer));
-    }
-     
-     
+
+int main(void){
+	
+	int i,err;
+
+	srand(time(NULL));
+
+	sem_init(&buf_mutex,0,1);
+	sem_init(&fill_count,0,0);
+
+	printf("Enter the number of Producers:");
+	scanf("%d",&prod_count);
+	producers = (pthread_t*) malloc(prod_count*sizeof(pthread_t));
+
+	printf("Enter the number of Consumers:");
+	scanf("%d",&con_count);
+	consumers = (pthread_t*) malloc(con_count*sizeof(pthread_t));
+
+	printf("Enter buffer capacity:");
+	scanf("%d",&buf_len);
+	buf = (int*) malloc(buf_len*sizeof(int));
+
+	sem_init(&empty_count,0,buf_len);
+
+	for(i=0;i<prod_count;i++){
+		err = pthread_create(producers+i,NULL,&producer,NULL);
+		if(err != 0){
+			printf("Error creating producer %d: %s\n",i+1,strerror(err));
+		}else{
+			printf("Successfully created producer %d\n",i+1);
+		}
+	}
+
+	for(i=0;i<con_count;i++){
+		err = pthread_create(consumers+i,NULL,&consumer,NULL);
+		if(err != 0){
+			printf("Error creating consumer %d: %s\n",i+1,strerror(err));
+		}else{
+			printf("Successfully created consumer %d\n",i+1);
+		}
+	}
+
+	for(i=0;i<prod_count;i++){
+		pthread_join(*(producers+i),NULL);
+	}
+	for(i=0;i<con_count;i++){
+		pthread_join(*(consumers+i),NULL);
+	}
+
+
+	return 0;
 }
