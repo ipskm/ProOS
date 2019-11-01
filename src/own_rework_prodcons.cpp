754b5e2ab537ccb1e86534ee1089960fe7b1c3b2
ipskm <ipskm@outlook.com>
2019-11-01 14:45:59 +0700

    change functions name

--- src/own_rework_prodcons.cpp
+++ src/own_rework_prodcons.cpp
@@ -27,20 +27,20 @@ void print(ostream &s)
 //
 //      Constants
 //
-const int num_adder = 10;               // จำนวนเทรดของ adder
-const int num_remover = 10;             // จำนวนเทรดของ remover
+const int num_append_ = 10;               // จำนวนเทรดของ append_
+const int num_remove = 10;             // จำนวนเทรดของ remove_
 const int delay_for_add = 10;           // in miliseconds
 const int delay_for_remove = 10;        // in miliseconds
 
-const int remover_max_wait_time = 200;  // in miliseconds - max time that a remover can wait for a item to be add_itemsd.
+const int max_remove_wait_time = 200;  // in miliseconds - max time that a remove_ can wait for a item to be add_itemsd.
 
-const int max_request = 100; // When adders has add_itemsd this quantity they will stop to add_items
+const int max_request = 100; // When append_s has add_itemsd this quantity they will stop to add_items
 const int buffer_size = 10;  // Maximum number of items that can be stored in buffers
 
 //
 //      Variables
 //
-atomic<int> num_removers_working(0);    // When there's no adder working the removers will stop, and the program will stop.
+atomic<int> num_append_working(0);    // When there's no append_ working the remove_s will stop, and the program will stop.
 queue<int> items;                       // The items queue, here we will store our items
 mutex xmutex;                           // Our mutex, without this mutex our program will cry
 
@@ -51,8 +51,8 @@ condition_variable is_not_empty;    // to indicate that our queue is not empty b
 //      Functions
 //
 
-//      add_items function, adder_id will add_items a item
-void add_items(int adder_id)
+//      add_items function, append__id will add_items a item
+void add_items(int append__id)
 {
     unique_lock<mutex> lock(xmutex);
     int item;
@@ -61,55 +61,55 @@ void add_items(int adder_id)
     item = items.size();
     items.push(item);
 
-    // print(stringstream() << "adder " << adder_id << " add_itemsd " << item << "\n"); // <-- uncomment to display adder add_items item
+    // print(stringstream() << "append_ " << append__id << " add_itemsd " << item << "\n"); // <-- uncomment to display append_ add_items item
     is_not_empty.notify_all();
 }
 
-//      remove_items function, remover_id will remove_items a item
-void remove_items(int remover_id)
+//      remove_items function, remove_id will remove_items a item
+void remove_items(int remove_id)
 {
     unique_lock<mutex> lock(xmutex);
     int item;
 
-    if (is_not_empty.wait_for(lock, chrono::milliseconds(remover_max_wait_time),
+    if (is_not_empty.wait_for(lock, chrono::milliseconds(max_remove_wait_time),
                               [] { return items.size() > 0; }))
     {
         item = items.front();
         items.pop();
 
-        // print(stringstream() << "remover " << remover_id << " remove_itemsd " << item << "\n"); // <-- uncomment to display removers remove_items item
+        // print(stringstream() << "remove_ " << remove_id << " remove_itemsd " << item << "\n"); // <-- uncomment to display remove_s remove_items item
         is_not_full.notify_all();
     }
 }
 
-//      adder function, this is the body of a adder thread
-void adder(int id)
+//      append_ function, this is the body of a append_ thread
+void append_(int id)
 {
-    ++num_removers_working;
+    ++num_append_working;
     for (int i = 0; i < max_request; ++i)
     {
         add_items(id);
         this_thread::sleep_for(chrono::milliseconds(delay_for_add));
     }
 
-    // print(stringstream() << "adder " << id << " has exited\n"); // <-- uncomment to display end adders thread
-    --num_removers_working;
+    // print(stringstream() << "append_ " << id << " has exited\n"); // <-- uncomment to display end append_s thread
+    --num_append_working;
 }
 
-//      remover function, this is the body of a remover thread
-void remover(int id)
+//      remove_ function, this is the body of a remove_ thread
+void remove_(int id)
 {
-    // Wait until there is any adder working
-    while (num_removers_working == 0)
+    // Wait until there is any append_ working
+    while (num_append_working == 0)
         this_thread::yield();
 
-    while (num_removers_working != 0 || items.size() > 0)
+    while (num_append_working != 0 || items.size() > 0)
     {
         remove_items(id);
         this_thread::sleep_for(chrono::milliseconds(delay_for_remove));
     }
 
-    // print(stringstream() << "remover " << id << " has exited\n"); // <-- uncomment to display end removers thread
+    // print(stringstream() << "remove_ " << id << " has exited\n"); // <-- uncomment to display end remove_s thread
 }
 
 //
@@ -122,22 +122,22 @@ int main()
     // unsync the I/O of C and C++.
     ios_base::sync_with_stdio(false);
 
-    // Start adder remover Program //
-    vector<thread> adders_and_removers;
+    // Start append_ remove_ Program //
+    vector<thread> append_s_and_remove_s;
 
-    // Create adders
-    for (int i = 0; i < num_adder; ++i)
-        adders_and_removers.push_back(thread(adder, i));
+    // Create append_s
+    for (int i = 0; i < num_append_; ++i)
+        append_s_and_remove_s.push_back(thread(append_, i));
 
-    // Create removers
-    for (int i = 0; i < num_remover; ++i)
-        adders_and_removers.push_back(thread(remover, i));
+    // Create remove_s
+    for (int i = 0; i < num_remove; ++i)
+        append_s_and_remove_s.push_back(thread(remove_, i));
 
-    // Wait for removers and adders to finish
-    for (auto &t : adders_and_removers)
+    // Wait for remove_s and append_s to finish
+    for (auto &t : append_s_and_remove_s)
         t.join();
 
-    // End adder Comsumer Program //
+    // End append_ Comsumer Program //
 
     // Record End time //
 
